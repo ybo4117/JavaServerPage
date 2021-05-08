ed2af3e2a35bd84af18da28083f1a90c29c2f283
ybo4117 <ybo4117@naver.com>
2021-05-08 20:33:00 +0900

    깃허브 연습중

--- BoardVel3/src/main/java/com/koreait/board3/BoardLoginDAO.java
+++ BoardVel3/src/main/java/com/koreait/board3/BoardLoginDAO.java
@@ -2,4 +2,5 @@ package com.koreait.board3;
 
 public class BoardLoginDAO {
 	//TODO: 로그인 DAO(데이터베이스 쿼리문이 들어가야할 곳)
+	
 }
--- JavaSpring/BoardVel2/src/main/java/com/koreait/board2/Database.java
+++
@@ -1,8 +0,0 @@
-package com.koreait.board2;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Database {
-	public static List db = new ArrayList();
-}
